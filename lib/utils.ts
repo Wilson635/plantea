9c1d9df1f3796adf6fdeba18a223d3fb22ab50eb
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2024-01-28 23:30:34 +0100

    upades

--- lib/utils.ts
+++ lib/utils.ts
@@ -62,6 +62,16 @@ export function formUrlQuery({ params, key, value }: UrlQueryParams) {
   )
 }
 
+export const formatPrice = (price: string) => {
+  const amount = parseFloat(price)
+  const formattedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount)
+
+  return formattedPrice
+}
+
 export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
   const currentUrl = qs.parse(params)
 
